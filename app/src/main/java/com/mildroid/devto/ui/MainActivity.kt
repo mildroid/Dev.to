18f3b2d12e9d4600573b6dc26568ce12fae9377e
Milad <mrmilad4753@gamil.com>
2022-05-21 10:07:30 +0430

    Initial commit

---
+++ app/src/main/java/com/mildroid/devto/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mildroid.devto.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
